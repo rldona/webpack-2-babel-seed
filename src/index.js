44a53ec64867e9dcdc0e27f0b7bc981f672b6097
Raúl López <rldona@gmail.com>
2017-05-16 13:38:28 +0200

    Added Babel and Sass loaders

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import sum from './sum.js';
+
+import './style.scss';
+
+const total = sum(1, 1);
+
+console.log(total);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: {
+          loader: 'babel-loader'
+        },
+        test: /\.js$/,
+        exclude: /(node_modules)/
+      },
+      {
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ],
+        test: /\.scss$/,
+      }
+    ]
+  }
+};
+
+module.exports = config;
